8a349a1c2105c548a6daafee3e707b654cf6d885
John S. Ryan <jtigger@infosysengr.com>
2013-09-25 15:43:42 -0700

    Extract git-grep config into separate file.
    - not only does this make it easier to share portions of config, it actually makes it easier to reason my total config (it's decomposed into cohesive units of config).

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
 cd ~
+# TODO: collect and make symlinks based on patterns instead of individual files
 ln -s .rucksack/git/.gitconfig .
+ln -s .rucksack/git/.gitconfig-grep .
 ln -s .rucksack/git/.bashrc-git .
 ln -s .rucksack/bash/.bashrc-aliases .
