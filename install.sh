fb344ec48b084665f198c9f140ea46de22514e10
John S. Ryan <jtigger@infosysengr.com>
2013-09-25 16:31:25 -0700

    Added reminder to source-in symlinks in .bashrc.

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
-cd ~
 # TODO: collect and make symlinks based on patterns instead of individual files
+cd ~
 ln -s .rucksack/git/.gitconfig .
 ln -s .rucksack/git/.gitconfig-grep .
 ln -s .rucksack/git/.bashrc-git .
 ln -s .rucksack/bash/.bashrc-aliases .
+
+# TODO: modify ~/.bashrc to source-in any symlink'ed files
