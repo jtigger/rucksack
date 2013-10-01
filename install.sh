4d14adb4fd32d161547b6f4c811b1a00d3567713
John S. Ryan <jtigger@infosysengr.com>
2013-10-01 07:35:52 -0700

    Added Ruby-specific bash config.  First is the 'be' alias.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 
+# TODO: make this installer idempotent
 # TODO: collect and make symlinks based on patterns instead of individual files
 cd ~
 ln -s .rucksack/git/.gitconfig .
 ln -s .rucksack/git/.gitconfig-grep .
 ln -s .rucksack/git/.bashrc-git .
 ln -s .rucksack/bash/.bashrc-aliases .
+ln -s .rucksack/ruby/.bashrc-ruby .
 
 # TODO: modify ~/.bashrc to source-in any symlink'ed files
