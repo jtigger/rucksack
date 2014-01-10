b5c787df8f58cb001e685e9873c6c171c7eee6ae
John S. Ryan <jtigger@infosysengr.com>
2014-01-10 09:10:29 -0800

    Now installer only symlinks files that required at a particular location.

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #/bin/bash
 
-# TODO: make this installer idempotent
 # TODO: collect and make symlinks based on patterns instead of individual files
 cd ~
 ln -s .rucksack/git/.gitconfig .
 ln -s .rucksack/git/.gitconfig-grep .
-ln -s .rucksack/git/.bashrc-git .
-ln -s .rucksack/bash/.bashrc-aliases .
-ln -s .rucksack/ruby/.bashrc-ruby .
+ln -s .rucksack/vim/.vimrc .
 
-# TODO: modify ~/.bashrc to source-in any symlink'ed files
+echo "for bash: Copy from .rucksack/bash/.bashrc.example..."
