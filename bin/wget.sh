f4851f50bfc715e9fe44af9485c6eebfc9f4dd65
John Ryan <jtigger@pm.me>
2026-02-21 10:10:33 -0800

    wget.sh passes all remaining params to wget

--- bin/wget.sh
+++ bin/wget.sh
@@ -4,4 +4,4 @@ mirror_site="--recursive --timestamping --level=inf --no-remove-listing --no-par
 make_navigable_from_disk="--page-requisites --convert-links --adjust-extension"
 with_retry="--tries=0 --retry-connrefused --waitretry=5 --read-timeout=60 --timeout=60"
 
-wget ${mirror_site} ${make_navigable_from_disk} ${with_retry} $1
+wget ${mirror_site} ${make_navigable_from_disk} ${with_retry} "$*"
